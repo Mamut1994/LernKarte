884047fb69301a82a85956a56b4e7ccf7170fa5a
haskir <mahmoudhaskir01@gmail.com>
2026-01-18 12:47:57 +0100

    Add Avalonia Projekt

---
+++ MeinApp/MeinApp/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MeinApp.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MeinApp/MeinApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MeinApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
